1e79bb3b7bc9348ab8272b3f31df29f00af64982
ODevGuilherme <gbaierledeferreira@gmail.com>
2024-09-09 21:22:50 -0300

    Instalando os plugins de camera e a interface de usuario da PWA Elements da Ionic

--- photo-gallery/src/main.ts
+++ photo-gallery/src/main.ts
@@ -1,6 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+
+// Importe o loader dos elementos PWA do Ionic
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+// Chame o carregador dos elementos personalizados antes de inicializar o módulo principal
+defineCustomElements(window);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
