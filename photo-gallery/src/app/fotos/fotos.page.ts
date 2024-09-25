feff9cbbfd50c95d88747a370de9275b1da0e1ee
Lucas Gregory Horn <lucasghornapp@gmail.com>
2024-09-24 23:28:52 -0300

    Ajustes nos layout dos botoes de acao

--- photo-gallery/src/app/fotos/fotos.page.ts
+++ photo-gallery/src/app/fotos/fotos.page.ts
@@ -7,39 +7,45 @@ import { PhotoService, UserPhoto } from '../services/photo.service';
   styleUrls: ['fotos.page.scss']
 })
 export class FotosPage {
-  public newComment: { [key: number]: string } = {};
   public likes: { [key: number]: number } = {};
-  public showComments: { [key: number]: boolean } = {}; 
+  public showComments: { [key: number]: boolean } = {};
+  public newComment: { [key: number]: string } = {};
 
-  constructor(public photoService: PhotoService) {}
+  constructor(public photoService: PhotoService) {
+    this.photoService.photos.forEach((photo, index) => {
+      this.likes[index] = 0;
+      this.showComments[index] = false;
+      this.newComment[index] = '';
+    });
+  }
 
   public showActionSheet(photo: UserPhoto, position: number) {
     this.photoService.showActionSheet(photo, position);
   }
 
-  // Função para adicionar comentário
-  public addComment(position: number) {
-    if (!this.newComment[position]) return; 
-  
-    if (!this.photoService.photos[position].comments) {
-      this.photoService.photos[position].comments = [];
-    }
-  
-    this.photoService.photos[position].comments!.push(this.newComment[position]);
-  
-    this.newComment[position] = '';
-  }
-  
-
-  public likePhoto(position: number) {
-    if (!this.likes[position]) {
+  public toggleLike(position: number) {
+    if (this.likes[position]) {
       this.likes[position] = 0;
+    } else {
+      this.likes[position] = 1;
     }
-
-    this.likes[position]++;
   }
 
   public toggleComments(position: number) {
     this.showComments[position] = !this.showComments[position];
+  }
+
+  public addComment(position: number) {
+    const photo = this.photoService.photos[position];
+    
+    if (!photo.comments || !Array.isArray(photo.comments)) {
+      photo.comments = [];
+    }
+
+    if (this.newComment[position]) {
+      photo.comments.push(this.newComment[position]);
+
+      this.newComment[position] = '';
+    }
   }
 }
