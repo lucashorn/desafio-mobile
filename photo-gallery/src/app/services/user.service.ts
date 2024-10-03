41cb0ef32c55a5eeed8814a0031e327c0f20be70
Lucas Gregory Horn <lucasghornapp@gmail.com>
2024-10-03 19:15:34 -0300

    AJustado para não buscar a propriedade se não existir o objeto

--- photo-gallery/src/app/services/user.service.ts
+++ photo-gallery/src/app/services/user.service.ts
@@ -18,20 +18,24 @@ export class UserService {
   }
 
   private getUserFromStorage() {
-    const userMetadata = JSON.parse(localStorage.getItem('sb-slbespgvkdhwxlqgbuvo-auth-token') || '{}').user.user_metadata;
-    let imageUrl = ''
-    if(userMetadata.profileImage){ 
-      const profileImage = userMetadata.profileImage
-      this.supabase.downLoadImage(`ProfileImages/${profileImage}`).then(image => {
-        this.user.profileImage = URL.createObjectURL(image.data!)
-      })
-    }
-
-    return {
-      id: userMetadata.sub, 
-      apelido: userMetadata.apelido,
-      email: userMetadata.email,
-      profileImage: imageUrl
-    };
+    const user = JSON.parse(localStorage.getItem('sb-slbespgvkdhwxlqgbuvo-auth-token') || '{}').user;
+    if (user && user.user_metadata) {
+      const userMetadata = user.user_metadata;
+      let imageUrl = ''
+      if(userMetadata.profileImage){ 
+        const profileImage = userMetadata.profileImage
+        this.supabase.downLoadImage(`ProfileImages/${profileImage}`).then(image => {
+          this.user.profileImage = URL.createObjectURL(image.data!)
+        })
+      }
+      return {
+        id: userMetadata.sub, 
+        apelido: userMetadata.apelido,
+        email: userMetadata.email,
+        profileImage: imageUrl
+      };
+  }
+  return;
+   
   }
 }
