9a3d3098616992e8eeca21270e6b066240adace8
WeligtonWeiss <weligtonweiss@gmail.com>
2024-10-03 22:44:52 -0300

    ajustando enviroments

--- photo-gallery/src/environments/environment.ts
+++ photo-gallery/src/environments/environment.ts
@@ -6,7 +6,6 @@ export const environment = {
   production: false,
   supabaseUrl: 'https://slbespgvkdhwxlqgbuvo.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNsYmVzcGd2a2Rod3hscWdidXZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjczNzMyOTIsImV4cCI6MjA0Mjk0OTI5Mn0.lI0_zf6JUMBGCkXp5YPd7T4BrdiwfAai-oAL7w6EpmA',
-  serviceRoleKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNsYmVzcGd2a2Rod3hscWdidXZvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNzM3MzI5MiwiZXhwIjoyMDQyOTQ5MjkyfQ.pyzvK04lyBwCadZPJ0I1FkPMZkxwef-9T3YEwzKPydQ'
 };
 
 /*
